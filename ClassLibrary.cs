76168c0a1cc8ec81cdcb8d8c8c8fe0ad7890d54b
kevinmartiin <nextgenallu@hotmail.com>
2024-02-12 16:14:56 +0100

    added methods to add entities to gameboard anda method to print out the gameboard.

--- ClassLibrary.cs
+++ ClassLibrary.cs
@@ -3,40 +3,55 @@ namespace classes
     
 
 class Entity{
-    int x;
-    int y;
+    public int x {get; set;}
+    public int y {get; set;}
+    public char symbol {get; set;}
 
-    public  Entity(int x, int y){
+    public  Entity(int x, int y, char symbol){
         this.x = x;
         this.y = y;
+        this.symbol = symbol;
     }
+        public virtual void Interact(Entity otherEntity)
+        {
+
+        }
 }
 
 class Player : Creature{
-    public Player (int x, int y, int speed, int strength, int health, int stamina) : base(x, y, speed, strength, health, stamina){
+    public Player (int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x, y, speed, strength, health, stamina, symbol){
 
     }
-}
+        public override void Interact(Entity otherEntity)
+        {
+            
+        }
+
+    }
 
 class Creature : Entity {
 
-    int strength; 
-    int health;
-    int stamina;
-    int speed;
+    int strength {get; set;} 
+    int health {get; set;}
+    int stamina {get; set;}
+    int speed {get; set;}
 
-    public Creature(int x, int y, int speed, int strength, int health, int stamina) : base(x,y) {
+    public Creature(int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x,y,symbol) {
         this.speed = speed;
         this.strength = strength;
         this.health = health;
         this.stamina = stamina;
     }
+        public override void Interact(Entity otherEntity)
+        {
+            
+        }
 
-}
+    }
 
 class Items : Entity{
 
-    public Items(int x, int y) : base(x,y) {
+    public Items(int x, int y, char symbol) : base(x,y,symbol) {
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,48 @@
 
 class Program
 {
-    static char[,] gameBoard = new char[10, 10];
+    static char[,] gameBoard = new char[12, 22];
     static void Main(string[] args)
     {
-        Player player1 = new Player(0,0, 2, 4, 10, 10);
-        Creature enemy1 = new Creature(5, 5, 2, 5, 10, 10);
-        Items item1 = new Items(3, 2);
+        Player player1 = new Player(0,0, 2, 4, 10, 10, 'P');
+        Creature enemy1 = new Creature(10, 16, 2, 5, 10, 10, 'E');
+        Items item1 = new Items(7, 6, 'I');
+
         
+
+        AddEntityToGameBoard(player1);
+        AddEntityToGameBoard(enemy1);
+        AddEntityToGameBoard(item1);
+
+        enemy1.Interact(player1);
+        item1.Interact(player1);
+
+        DrawGameBoard();
+
+
     }
+
+    static void AddEntityToGameBoard(Entity entity)
+    {
+        gameBoard[entity.x, entity.y] = entity.symbol;
+    }
+
+    static void DrawGameBoard()
+{
+    for (int i = 0; i < gameBoard.GetLength(0); i++)
+    {
+        for (int j = 0; j < gameBoard.GetLength(1); j++)
+        {
+            if (gameBoard[i, j] == '\0')
+            {
+                Console.Write("- ");
+            }
+            else
+            {
+                Console.Write(gameBoard[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
 }
