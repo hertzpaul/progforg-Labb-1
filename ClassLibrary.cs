af47cac73aac2226b850f4a23b81608b51482ef4
kevinmartiin <nextgenallu@hotmail.com>
2024-02-13 11:55:09 +0100

    added player movement and a main menu

--- ClassLibrary.cs
+++ ClassLibrary.cs
@@ -1,58 +1,104 @@
 namespace classes
 {
-    
 
-class Entity{
-    public int x {get; set;}
-    public int y {get; set;}
-    public char symbol {get; set;}
 
-    public  Entity(int x, int y, char symbol){
-        this.x = x;
-        this.y = y;
-        this.symbol = symbol;
-    }
+    class Entity
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public char symbol { get; set; }
+
+        public Entity(int x, int y, char symbol)
+        {
+            this.x = x;
+            this.y = y;
+            this.symbol = symbol;
+        }
         public virtual void Interact(Entity otherEntity)
         {
 
         }
-}
-
-class Player : Creature{
-    public Player (int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x, y, speed, strength, health, stamina, symbol){
-
     }
+
+    class Player : Creature
+    {
+        public Player(int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x, y, speed, strength, health, stamina, symbol)
+        {
+
+        }
         public override void Interact(Entity otherEntity)
         {
-            
+        }
+
+        public void HandleMovement(ConsoleKey key, char[,] gameBoard)
+        {
+            int newX = x;
+            int newY = y;
+
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    newX--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    newX++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    newY--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    newY++;
+                    break;
+                default:
+                    return;
+            }
+            try
+            {
+                if (newX < 0 || newX >= gameBoard.GetLength(0) || newY < 0 || newY >= gameBoard.GetLength(1))
+                {
+                    throw new ArgumentOutOfRangeException("Cannot move outside the bounds of the game board.");
+                }
+
+                gameBoard[x, y] = '\0';
+                x = newX;
+                y = newY;
+                gameBoard[x, y] = symbol;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+
+    class Creature : Entity
+    {
+
+        int strength { get; set; }
+        int health { get; set; }
+        int stamina { get; set; }
+        int speed { get; set; }
+
+        public Creature(int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x, y, symbol)
+        {
+            this.speed = speed;
+            this.strength = strength;
+            this.health = health;
+            this.stamina = stamina;
+        }
+        public override void Interact(Entity otherEntity)
+        {
+
         }
 
     }
 
-class Creature : Entity {
+    class Items : Entity
+    {
 
-    int strength {get; set;} 
-    int health {get; set;}
-    int stamina {get; set;}
-    int speed {get; set;}
-
-    public Creature(int x, int y, int speed, int strength, int health, int stamina, char symbol) : base(x,y,symbol) {
-        this.speed = speed;
-        this.strength = strength;
-        this.health = health;
-        this.stamina = stamina;
-    }
-        public override void Interact(Entity otherEntity)
+        public Items(int x, int y, char symbol) : base(x, y, symbol)
         {
-            
+
         }
-
     }
-
-class Items : Entity{
-
-    public Items(int x, int y, char symbol) : base(x,y,symbol) {
-
-    }
-}
 }
--- Program.cs
+++ Program.cs
@@ -5,11 +5,83 @@ class Program
     static char[,] gameBoard = new char[12, 22];
     static void Main(string[] args)
     {
-        Player player1 = new Player(0,0, 2, 4, 10, 10, 'P');
+        // Player player1 = new Player(0, 0, 2, 4, 10, 10, 'P');
+        // Creature enemy1 = new Creature(10, 16, 2, 5, 10, 10, 'E');
+        // Items item1 = new Items(7, 6, 'I');
+
+
+
+        // AddEntityToGameBoard(player1);
+        // AddEntityToGameBoard(enemy1);
+        // AddEntityToGameBoard(item1);
+
+        // enemy1.Interact(player1);
+        // item1.Interact(player1);
+
+        // DrawGameBoard();
+
+        // ConsoleKeyInfo keyInfo;
+        // do
+        // {
+        //     keyInfo = Console.ReadKey(true);
+        //     player1.HandleMovement(keyInfo.Key, gameBoard);
+        //     DrawGameBoard();
+        // } while (keyInfo.Key != ConsoleKey.Escape);
+
+        bool inMainMenu = true;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("---------------*****---------------");
+            Console.WriteLine("Welcome to SalesAdventure3000!");
+            Console.WriteLine("---------------*****---------------");
+            Console.WriteLine("\nPress '1' to start the game\nPress '2' to read the game info and instructions\nPress 'Escape' to quit the game");
+            
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch(keyInfo.KeyChar)
+            {
+                case '1':
+                inMainMenu = false;
+                StartGame();
+                break;
+                case '2':
+                GameInfo();
+                break;
+                case (char)ConsoleKey.Escape:
+                Environment.Exit(0);
+                break;
+                default:
+                Console.WriteLine("Invalid input");
+                break;
+            }
+
+        }while (inMainMenu);
+
+    }
+    static void GameInfo()
+    {
+        Console.Clear();
+        Console.WriteLine("----------------------------------------------------------------*******---------------------------------------------------------------");
+        Console.WriteLine("\nSo you chose to be a pleb and read the gameinfo");
+        Console.WriteLine("\nThis game is a combat game where you as a player will need to pickup items in order to challenge various monsters on the board");
+        Console.WriteLine("\nPlayers will be marked as 'P' on the board. Items will be marked as 'I' and monsters will be marked as 'E'");
+        Console.WriteLine("\nTo pickup items you will need to stand on the same position as the item");
+        Console.WriteLine("\nSome items will be needed to equipped and some will be consumed on use.");
+        Console.WriteLine("\nTo challenge and fight monsters you will need to stand in the same spot as the monster to initiate combat");
+        Console.WriteLine("\nTo move around the board you will use Arrow keys");
+        Console.WriteLine("----------------------------------------------------------------*******---------------------------------------------------------------");
+        Console.WriteLine("\nPress 'Enter' to return to Main Menu");
+        Console.ReadLine();
+    }
+
+    static void StartGame()
+    {
+        Player player1 = new Player(0, 0, 2, 4, 10, 10, 'P');
         Creature enemy1 = new Creature(10, 16, 2, 5, 10, 10, 'E');
         Items item1 = new Items(7, 6, 'I');
 
-        
+
 
         AddEntityToGameBoard(player1);
         AddEntityToGameBoard(enemy1);
@@ -20,7 +92,13 @@ class Program
 
         DrawGameBoard();
 
-
+        ConsoleKeyInfo keyInfo;
+        do
+        {
+            keyInfo = Console.ReadKey(true);
+            player1.HandleMovement(keyInfo.Key, gameBoard);
+            DrawGameBoard();
+        } while (keyInfo.Key != ConsoleKey.Escape);
     }
 
     static void AddEntityToGameBoard(Entity entity)
@@ -29,21 +107,22 @@ class Program
     }
 
     static void DrawGameBoard()
-{
-    for (int i = 0; i < gameBoard.GetLength(0); i++)
     {
-        for (int j = 0; j < gameBoard.GetLength(1); j++)
+        Console.Clear();
+        for (int i = 0; i < gameBoard.GetLength(0); i++)
         {
-            if (gameBoard[i, j] == '\0')
+            for (int j = 0; j < gameBoard.GetLength(1); j++)
             {
-                Console.Write("- ");
+                if (gameBoard[i, j] == '\0')
+                {
+                    Console.Write("- ");
+                }
+                else
+                {
+                    Console.Write(gameBoard[i, j] + " ");
+                }
             }
-            else
-            {
-                Console.Write(gameBoard[i, j] + " ");
-            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
     }
-}
 }
